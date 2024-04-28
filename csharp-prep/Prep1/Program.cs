0849ce0042e834f7a6945504b2092918721808ad
abecerrile <bec23017@byui.edu>
2024-04-28 17:03:20 -0600

    update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -10,7 +10,7 @@ class Program
         Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();   
 
-        Console.WriteLine($"Your name is: {lastname}, {firstname}{lastname}");
+        Console.WriteLine($"Your name is: {lastname}, {firstname} {lastname}");
     }
 
 }
