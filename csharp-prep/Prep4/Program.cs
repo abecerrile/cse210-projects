04ed7b0cf0d329b35dd1d857671967715bec1a57
abecerrile <bec23017@byui.edu>
2024-04-28 17:41:19 -0600

    UPDATE

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num == 0)
+            {
+                break;
+            }
+            numbers.Add(num);
+        }
+
+        int total = numbers.Sum();
+
+        double average = numbers.Average();
+
+        int maximum = numbers.Max();
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maximum}");
     }
 }
