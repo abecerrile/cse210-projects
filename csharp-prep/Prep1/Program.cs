fc8db3c1182d037674eb96f7c558d0a915c89edb
abecerrile <bec23017@byui.edu>
2024-04-28 16:40:54 -0600

    UPDATE

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,15 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Prompt user for their first name
         Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
+        string first = Console.ReadLine();
 
-        // Prompt user for their last name
         Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
+        string last = Console.ReadLine();
 
-        // Display the formatted name
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
