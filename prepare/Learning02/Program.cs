f0a4adc610b12a3c2c226e3227bdb2dabedeb0fd
abecerrile <bec23017@byui.edu>
2024-04-30 18:18:22 -0600

    update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,23 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
+
+        // Displaying job details individually
+        job1.Display();
+        job2.Display();
+
+        // Creating resume
+        Resume myResume = new Resume("Allison Rose");
+
+        // Adding jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Displaying resume details
+        myResume.Display();
     }
 }
